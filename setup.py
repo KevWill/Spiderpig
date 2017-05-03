c0f1f7d6382319d7c51f63c07bb22d233f31d3e2
Kevin Willemsen <kevin.willemsen@gmail.com>
2017-05-03 16:28:46 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Spiderpig',
+    version='1.0',
+    packages=['requests'],
+    url='https://github.com/KevWill/Spiderpig.git',
+    license='',
+    author='Kevin Willemsen',
+    author_email='kevin.willemsen@gmail.com',
+    description='Spiderpig finds urls in messages you feed it, and expands them efficiëntly.'
+)
